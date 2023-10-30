c96ee292079d80174bd847079e14afc46f91fa23
Zwx-tech <71787137+Zwx-tech@users.noreply.github.com>
2023-10-30 23:06:52 +0100

    added speedway and cart classes

--- classes.js
+++ classes.js
@@ -1,11 +1,38 @@
 class Cart {
+    ctx; // current canvas context
+    pos = {x: 0, y: 0}; // {x, y}
+    vel = 2; // 
+    angle = 0; // radians
+    angle_vel = ((2 * Math.PI) / 360) * 5;
+
+
+    constructor(ctx, startingPos) {
+        this.ctx = ctx;
+        this.pos = startingPos;
+    }
+
+    update() {
+        this.pos.x += this.vel * Math.cos(this.angle);
+        this.pos.y += this.vel * Math.sin(this.angle);
+        this.draw();
+    }
+
+    draw() {
+        const [w, h] = [24, 12]
+        this.ctx.beginPath();
+        this.ctx.rect(this.pos.x, this.pos.y, w, h);
+        this.ctx.fillStyle = "#4287f5";
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
 
 }
 
 class Speedway {
-    ctx; // curent canvas context
+    ctx; // current canvas context
     w; // width of speedway
     h; // height of speedway
+
     constructor(ctx, w, h) {
         this.ctx = ctx;
         this.w = w;
@@ -13,8 +40,30 @@ class Speedway {
     }
 
     render() {
+        let offset = 10;
         this.ctx.beginPath();
-        this.ctx.moveTo(this.h / 2, this.h/2 + 10);
-        this.ctx.arc(this.h / 2, this.h/2 + 10, this.h/2, Math.PI / 2, Math.PI / 2 + Math.PI);
+        this.ctx.rect(0, 0, this.w, this.h);
+        this.ctx.fillStyle = "#474747";
+        this.ctx.fill();
+        this.ctx.closePath();
+        this.ctx.beginPath();
+        this.ctx.arc(this.h / 2, this.h/2, this.h/2 - 2 * offset, Math.PI / 2, Math.PI / 2 + Math.PI);
+        this.ctx.lineTo(this.w - this.h/2, 2 * offset);
+        this.ctx.arc(this.w - this.h/2, this.h/2, this.h/2 - 2 * offset, Math.PI / 2 + Math.PI, Math.PI / 2);
+        this.ctx.lineTo(this.h/2, this.h - offset * 2);
+        this.ctx.fillStyle = "#0aab23";
+        this.ctx.fill();
+        this.ctx.closePath();
+        offset = 100;
+        this.ctx.beginPath();
+        this.ctx.arc(this.h / 2, this.h/2, this.h/2 - 2 * offset, Math.PI / 2, Math.PI / 2 + Math.PI);
+        this.ctx.lineTo(this.w - this.h/2, 2 * offset);
+        this.ctx.arc(this.w - this.h/2, this.h/2, this.h/2 - 2 * offset, Math.PI / 2 + Math.PI, Math.PI / 2);
+        this.ctx.lineTo(this.h/2, this.h - offset * 2);
+        this.ctx.fillStyle = "#474747";
+        this.ctx.fill();
+        this.ctx.closePath();
     }
 }
+
+export { Speedway, Cart };
--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-import "./classes";
+import { Cart, Speedway } from './classes.js'
 
-const  canvas = document.querySelector('cavnas');
+const canvas = document.querySelector('canvas');
+console.log(document)
 const ctx = canvas.getContext('2d');
-const speedway = new Speedway(ctx);
+const speedway = new Speedway(ctx, canvas.width, canvas.height);
+const carts = [new Cart(ctx, {x: canvas.width / 2, y: canvas.height - 10 - 100})]
+
+
+function update(step) {
+    speedway.render();
+    carts.forEach(c => c.update());
+    requestAnimationFrame(update)
+}
+
+requestAnimationFrame(update);
