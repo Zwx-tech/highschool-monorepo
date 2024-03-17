a6cb676cc51dff5f1e336b4f36e2daf6d5cf3332
Zwx-tech <71787137+Zwx-tech@users.noreply.github.com>
2024-03-17 16:23:32 +0100

    v1.0

--- projects/project-cars/serwer.js
+++ projects/project-cars/serwer.js
@@ -23,8 +23,21 @@ app.use(express.urlencoded({
 app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', hbs({
     defaultLayout: 'main.hbs',
+    extname: '.hbs',
+    partialsDir: "views/partials",
     helpers: {
-         
+        "isCarSelected": function (id, options) {
+            if (options.data.root.selectedCar == id) {
+                return options.fn(this);
+            } 
+            return options.inverse(this);
+        },
+        "equal": function (a, b, options) {
+            if (a === b) {
+                return 'selected';
+            } 
+            return '';
+        }
     }
 }));
 app.set('view engine', 'hbs');
@@ -44,14 +57,90 @@ function parseCar(car) {
         benzyna: parseCarValues(car.benzyna),
         uszkodzony: parseCarValues(car.uszkodzony),
         naped: parseCarValues(car.naped)
-    }   
+    };   
     return {
         ...parsedData,
         alertData: JSON.stringify(parsedData, null, 5)
     }    
 }
 
-const templateDbOjbect = {
+function fetchAllCars() {
+    return new Promise((resolve, reject) => {
+        db.find({}, (err, cars) => {
+            if (err) {
+                reject(err);
+                return;
+            } 
+            const parsedCars = cars.map(car => parseCar(car));
+            resolve(parsedCars);
+        });
+    });
+}
+
+function delCarById(id) {
+    return new Promise((resolve, reject) => {
+        db.remove({_id: id}, (err, n) => {
+            if (err) {
+                reject(err);
+                return;
+            } 
+            resolve();
+        });
+    });
+}
+
+function delCarsByIds(idList) {
+    const deleteCount = idList.length;
+    return new Promise(async (resolve, reject) => {
+        for(const id of idList) {
+            try {
+                await delCarById(id);
+            } catch {
+                deleteCount -= 1;
+            }
+        }
+        resolve(deleteCount);
+    });
+}
+
+function getIdFromReq(query) {
+    for(const [key, value] of query) {
+        if(value === '') {
+            return key
+        }
+    }  
+    return null;
+}
+
+function parseSelectEntry(entry) {
+    if(entry == "true") return true
+    if(entry == "false") return false
+    return null
+}
+
+function getCarDataFromReq(body) {
+    return {
+        ubezpiecznie: parseSelectEntry(body.ubezpiecznie),
+        benzyna: parseSelectEntry(body.benzyna),
+        uszkodzony: parseSelectEntry(body.uszkodzony),
+        naped: parseSelectEntry(body.naped)
+    }
+}
+
+
+function updateCarById(id, editedCar) {
+    console.log(editedCar)
+    return new Promise((resolve, reject) => {
+        db.update({ _id: id }, { $set: {...editedCar} }, {}, (err, n) => {
+            if (err) {
+                reject(err);
+                return;
+            } 
+            resolve();
+        });
+    });
+}
+const templateDbObject = {
     ubezpiecznie: false,
     benzyna: false,
     uszkodzony: false,
@@ -69,29 +158,63 @@ app.get("/add", function (req, res) {
     res.render('add.hbs'); 
 })
 
-//* LIST route
-app.get("/list", function (req, res) {
-    db.find({}, (err, cars) => {
-        const parsedCars = cars.map(car => parseCar(car));
-        res.render('list.hbs', { cars: parsedCars }); 
+app.post("/add", function (req, res) {
+    const carData = Object.keys(req.body);
+    const car = structuredClone(templateDbObject);
+    for(const key of carData) {
+        car[key] = true;
+    }
+    db.insert(car, (err, newDoc) => {
+        res.render('add.hbs', {alert: `new car with id = ${newDoc._id} added to database`});
     })
+});
+
+//* LIST route
+app.get("/list", async function (req, res) {
+    res.render('list.hbs', { cars: await fetchAllCars() }); 
 })
 
 //* DELETE route
-app.get("/delete", function (req, res) {
-    res.render('delete.hbs');
+app.get("/delete", async function (req, res) {
+    res.render('delete.hbs', { cars: await fetchAllCars() }); 
+})
+
+app.get('/deleteAll', function (req, res) {
+    db.remove({}, { multi: true }, async (err, n) => {
+        res.render('delete.hbs', { deletedCars: n, cars: await fetchAllCars() });
+    })
+}); 
+
+app.get('/deleteSelected', async function (req, res) {
+    const carsToDel = Object.keys(req.query);
+    res.render('delete.hbs', {cars: await fetchAllCars(), deletedCars: await delCarsByIds(carsToDel)});
+}); 
+
+app.get('/deleteSingle', async function (req, res) {
+    const inputs = Object.entries(req.query);
+    for(const [key, value] of inputs) {
+        if(value === '') {
+            await delCarById(key)
+            break;
+        }
+    }
+    res.render('delete.hbs', {cars: await fetchAllCars(), deletedCars: 1});
+}); 
+
+//* EDIT route
+app.get("/edit", async function (req, res) {
+    const query = Object.entries(req.query);
+    const selectedCar = getIdFromReq(query);
+    res.render('edit.hbs', {cars: await fetchAllCars(), selectedCar: selectedCar});
 })
 
 //* EDIT route
-app.get("/edit", function (req, res) {
-    res.render('edit.hbs');
-})
-
-app.post('/add', function (req, res) {
-    const carData = structuredClone(templateDbOjbect);
-    1
-    console.log(carData);
-    res.render('add.hbs'); 
+app.post("/update", async function (req, res) {
+    const resData = Object.entries(req.body);
+    const carId = getIdFromReq(resData);
+    const carData = getCarDataFromReq(req.body);
+    await updateCarById(carId, carData);
+    res.redirect('/edit')
 })
 
 //* static
