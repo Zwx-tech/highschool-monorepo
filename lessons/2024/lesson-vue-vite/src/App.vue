6bfcdedeb767bed6abf852795f9d48e917f92a28
Zwx-tech-school <sdebicki42@uczen.tl.krakow.pl>
2024-10-23 11:45:43 +0200

    vue vite 1.0

---
+++ lessons/2024/lesson-vue-vite/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-4">
+    <!-- <Lesson1 /> -->
+    <Lesson2 />
+  </div>
+</template>
+
+<script setup>
+  import Lesson1 from './lessons/Lesson1.vue';
+  import Lesson2 from './lessons/Lesson2.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ lessons/2024/lesson-vue-vite/src/lessons/Lesson2.vue
@@ -0,0 +1,29 @@
+<script setup>
+import carData from "@data/cars.json"
+import RoboCard from "@components/Lesson1/RoboCard.vue";
+import { computed, reactive } from "vue";
+
+const cars = reactive({ list: [...carData], count: 5 });
+
+const carList = computed(() => {
+    return cars.list.filter(c => c.id < cars.count)
+});
+
+function handlePagination() {
+    cars.count = (cars.count + 5) % cars.list.length;
+}
+
+</script>
+<template>
+    <div class="p-8">
+        <h2 class="text-2xl"> Text from json 3/4/5</h2>
+        <div class="flex gap-4 mt-4 flex-wrap">
+            <RoboCard v-for="car in carList" :car="car" :showInfo="false" />
+        </div>
+        <button class="bg-gray-700 text-white rounded-lg mt-2 text-sm font-bold px-6 py-2 uppercase"
+            @click="handlePagination">Pokaż więcej</button>
+    </div>
+</template>
+<style lang="tailwind/css">
+
+</style>
